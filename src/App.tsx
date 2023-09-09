6e62e3b17845e0652564115d2a9ad51f5333c6e3
Giorgi Abashidzee <abashidze.official@gmail.com>
2023-09-09 23:25:12 +0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { useState } from 'react';
 import './App.css';
+import daytime from './images/mobile/daytime.png';
+import nighttime from './images/mobile/bg-image-nighttime.jpg'
+import './styles.css';
+import sun from './images/desktop/icon-sun.svg';
+import moon from './images/desktop/icon-moon.svg'
 
 function App() {
+  const [isMorning, setIsMorning] = useState(true); // 
+
+  const hanldeChangeThem = () => {
+    setIsMorning(!isMorning); // funqcia achens da aqrobs sun/moons
+  }
   return (
-    <div className="App">
+    <div className={`container ${isMorning ? 'daytime-icon' : 'nighttime-icon'}`}>
+      <img src={isMorning ? daytime : nighttime}
+        alt="daytime" className="daytime-icon"
+      />
+      <div className="contrast">
+        <button className="them" onClick={hanldeChangeThem}>
+          <img src={isMorning ? sun : moon} alt="sun" className="sun" />
+          <span className={isMorning ? 'sun-text' : 'moon-text'}>
+            {isMorning ? 'GOOD MORNING' : 'GOOD EVENING'}
+          </span>
+        </button>
+      </div>
 
     </div >
   );
